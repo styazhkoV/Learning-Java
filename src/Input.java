6684821198d6a7bc7f69c4044659b42f4baed805
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-04-09 14:56:13 +0600

    Do Whille

--- src/Input.java
+++ src/Input.java
@@ -2,12 +2,10 @@ import java.util.Scanner;
 
 public class Input {
     public static void main(String[] args) {
-        /*String str = "asdasdasdas";// объект
-        String s = new String("sdfdsdsgdsfgsd");// Экземпляр класса*/
-        Scanner b = new Scanner(System.in);
-        System.out.println("введите текст");
-        String x = b.nextLines();
-        System.out.println("вы ввели" +b);
+        Scanner s = new Scanner(System.in);
+        System.out.println("Введите, что нибудь");
+        int x = s.nextInt();
+        System.out.println("Вы ввели" + x);
     }
 }
 
