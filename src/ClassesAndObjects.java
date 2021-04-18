02f84874959ee0074c24b9316818549e198193bf
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-04-18 12:24:35 +0600

    ClassesAndObjects udp

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -3,18 +3,29 @@ public class ClassesAndObjects {
         Persone persone1 = new Persone();
         persone1.name = "Bob";
         persone1.age = 50;
-        System.out.println("Меня зовут "+ persone1.name +","+ "мне" + persone1.age+"лет");
+        persone1.sayHello(); //Вызов метода Speek
         Persone persone2 = new Persone();
         persone2.name = "Alex";
-        persone2.age = 21;
-        System.out.println("Меня зовут "+ persone2.name +","+ "мне" + persone2.age+"лет");
+        persone2.age = 25;
+        persone2.speek();
     }
 }
-class Persone{
+
+class Persone {
     //У класса могут быть:
     //1.данные(поля),
     //2. действия которые он может совершать (методы)
     String name;
     int age;
 
+    void speek() { //метод
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Меня зовут " + name + ", мне " + age + " лет");
+        }
+
+    }
+
+    public void sayHello() {
+        System.out.println("Привет!");
+    }
 }
