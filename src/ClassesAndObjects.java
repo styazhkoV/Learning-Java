e58fcc0f428b051f74cd3df34c5c0c3674d4aa4a
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-04-18 14:36:44 +0600

    ClassesAndObjects udp_1

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -1,17 +1,19 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Persone persone1 = new Persone();
-        persone1.name = "Bob";
-        persone1.age = 50;
+        persone1.setNameAndAge("Роман",33);
+        String s1 = "Вова";
+        //persone1.age = 50;
         persone1.sayHello(); //Вызов метода Speek
         Persone persone2 = new Persone();
-        persone2.name = "Alex";
-        persone2.age = 25;
+        persone2.setNameAndAge(s1,22);
+        //persone2.age = 25;
         persone2.speek();
-        int year1 = persone1.calculateYersToReiterment();
+        persone1.speek();
+        /*int year1 = persone1.calculateYersToReiterment();
         int year2 = persone2.calculateYersToReiterment();
         System.out.println("первому человеку до пенсии " + year1 + " лет");
-        System.out.println("второму человеку до пенсии " + year2 + " лет");
+        System.out.println("второму человеку до пенсии " + year2 + " лет");*/
     }
 }
 
@@ -22,9 +24,16 @@ class Persone {
     String name;
     int age;
 
+    void setNameAndAge(String username, int userage ){
+       name = username;
+       age = userage;
+    }
+
     int calculateYersToReiterment(){
         int yers = 65 - age;
         return yers;
+
+
     }
     void speek() { //метод
         for (int i = 0; i < 3; i++) {
